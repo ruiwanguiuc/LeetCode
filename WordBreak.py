045444e86b6f64021dfa432756085efa7dbf2511
Rui Wang <ruiwang@MacBookPro-AC7F3EE5F326-2.local>
2014-10-04 22:00:15 -0700

    finished word_break.py

--- WordBreak.py
+++ WordBreak.py
@@ -1,13 +1,21 @@
+# source of this problem:
+# http://thenoisychannel.com/2011/08/08/retiring-a-great-interview-problem
 class WordBreak:
 
 	@staticmethod
 	def word_break(word_to_break, word_set):
 		if not word_to_break:
 			return ""
+		if word_to_break in word_set:
+			return word_to_break
 		for i in range(len(word_to_break)):
-			if word_to_break[:i+1] in word_set:
-				
-		return word_to_break
+			first_half = word_to_break[:i+1]
+			if first_half in word_set:
+				remaining = word_to_break[i+1:]
+				remaining_word_segment = WordBreak.word_break(remaining, word_set)
+				if remaining_word_segment:
+					return first_half + " " + remaining_word_segment
+		return ""
 
 if __name__ == "__main__":
-	print WordBreak.word_break('applepie', set(['apple', 'pie', 'and', 'person']))
+	print WordBreak.word_break('applepie', set(['apple', 'pie', 'app', 'person']))
